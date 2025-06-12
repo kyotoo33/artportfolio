75c765249b41e7216cf87498425bc98b3b45f5e3
kyotoo33 <kyotoo33@gmail.com>
2025-06-12 12:11:24 +0800

    some fixes

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -87,11 +87,8 @@ export function Portfolio() {
                     />
                   ) : item.type === "youtube" ? (
                     <iframe 
-                      width="1020" 
-                      height="630" 
                       src={`https://www.youtube.com/embed/${item.videoId}?autoplay=1`}
                       title="YouTube video player" 
-                      frameBorder="0" 
                       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
                       referrerPolicy="strict-origin-when-cross-origin" 
                       allowFullScreen
